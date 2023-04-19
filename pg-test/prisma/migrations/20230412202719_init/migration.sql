d6643234764c08205aeabb95b5ed7b03e47f0beb
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-19 15:49:45 -0300

    renamed backend folder

--- pg-test/prisma/migrations/20230412202719_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_receiver_uuid_fkey";
-
--- AlterTable
-ALTER TABLE "messages" ALTER COLUMN "receiver_uuid" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_receiver_uuid_fkey" FOREIGN KEY ("receiver_uuid") REFERENCES "users"("uuid") ON DELETE SET NULL ON UPDATE CASCADE;
