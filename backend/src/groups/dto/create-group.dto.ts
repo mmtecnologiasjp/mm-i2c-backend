f7de564151a8471ec6efd05ea2602db64e563660
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-27 13:37:30 -0300

    Add validation to CreateGroupDto properties

--- backend/src/groups/dto/create-group.dto.ts
+++ backend/src/groups/dto/create-group.dto.ts
@@ -1,1 +1,16 @@
-export class CreateGroupDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateGroupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  description: string | null;
+
+  @IsString()
+  image_url: string | null;
+
+  @IsString()
+  creator_uuid: string;
+}
