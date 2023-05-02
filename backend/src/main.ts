f87b3a2f9eaaed79dc0fdab4432205f01a2cead8
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-02 17:36:22 -0300

    Add Swagger documentation to NestJS backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  const PORT = process.env.PORT ?? 2000;
+
+  const config = new DocumentBuilder()
+    .setTitle('MM Chat')
+    .setDescription('MM Chat API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const path = 'api';
+  SwaggerModule.setup(path, app, document);
+
+  const PORT = process.env.PORT ?? 4000;
   await app.listen(PORT, () =>
     console.log(`[MM CHAT] RUNNING ON PORT ${PORT}`),
   );
