8f6f1b222f042fb4a51ff294ed1fe4e49bee1cbc
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-02 17:36:13 -0300

    Delete app.controller.spec.ts, app.controller.ts, and app.service.ts files from backend/src

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [UsersModule, GroupsModule],
 })
 export class AppModule {}
