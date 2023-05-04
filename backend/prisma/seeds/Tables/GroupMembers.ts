6c447adf6f383200e0bcc6baab6a17a93dd6c9e4
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-04 15:12:34 -0300

    Add role field to group members seeding script

--- backend/prisma/seeds/Tables/GroupMembers.ts
+++ backend/prisma/seeds/Tables/GroupMembers.ts
@@ -1,13 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, RoleEnum } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export class GroupMembersSeeds {
   static async execute() {
     for (let i = 0; i < 2; i++) {
+      const role = i === 0 ? RoleEnum.Admin : RoleEnum.Member;
+
       const groupMember = await prisma.groupMember.create({
         data: {
-          group_id: `01`,
+          group_uuid: `01`,
           user_uuid: `0${i + 1}`,
+          role,
         },
       });
       console.log(`Created groupMember with id: ${groupMember.uuid}`);
