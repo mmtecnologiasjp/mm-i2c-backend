153f291ee97bad3bb25ef5a26053214cddbd8bad
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-04 17:15:34 -0300

    Add Swagger decorators to GroupMembersController actions

--- backend/src/group-members/dto/create-group-member.dto.ts
+++ backend/src/group-members/dto/create-group-member.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { RoleEnum } from '@prisma/client';
 import {
   IsRequiredEnum,
   IsRequiredString,
 } from 'src/utils/class-validator/decorators';
+import { generateUUID } from 'src/utils/uuid/generateUUID';
 
 export class CreateGroupMemberDto {
   @IsRequiredString()
+  @ApiProperty({ default: generateUUID() })
   readonly group_uuid: string;
 
   @IsRequiredString()
+  @ApiProperty({ default: generateUUID() })
   readonly user_uuid: string;
 
   @IsRequiredEnum(RoleEnum)
+  @ApiProperty({ enum: RoleEnum })
   readonly role: RoleEnum;
 }
--- backend/src/group-members/entities/group-member.entity.ts
+++ backend/src/group-members/entities/group-member.entity.ts
@@ -1,1 +1,30 @@
-export class GroupMember {}
+import { GroupMember as PrismaGroupMember, RoleEnum } from '@prisma/client';
+import {
+  ApiPropertyDeletedAt,
+  ApiPropertyEnum,
+  ApiPropertyTimestamp,
+  ApiPropertyUUID,
+} from 'src/utils/swagger/dto-decorators';
+
+export class GroupMember implements PrismaGroupMember {
+  @ApiPropertyUUID()
+  uuid: string;
+
+  @ApiPropertyUUID()
+  user_uuid: string;
+
+  @ApiPropertyUUID()
+  group_uuid: string;
+
+  @ApiPropertyEnum({ Enum: RoleEnum, Default: RoleEnum.Admin })
+  role: RoleEnum;
+
+  @ApiPropertyTimestamp()
+  created_at: Date;
+
+  @ApiPropertyTimestamp()
+  updated_at: Date;
+
+  @ApiPropertyDeletedAt()
+  deleted_at: Date | null;
+}
