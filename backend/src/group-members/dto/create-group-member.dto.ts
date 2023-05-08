c0c10fb48c6bf4611ef289f5841c5bb576a0babf
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-08 10:36:13 -0300

    Refactor CreateGroupMemberDto with Swagger and class-validator enhancements

--- backend/src/group-members/dto/create-group-member.dto.ts
+++ backend/src/group-members/dto/create-group-member.dto.ts
@@ -1,25 +1,27 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { RoleEnum } from '@prisma/client';
 import { Validate } from 'class-validator';
 import {
   IsRequiredEnum,
   IsRequiredString,
 } from 'src/utils/class-validator/decorators';
-import { generateUUID } from 'src/utils/uuid/generateUUID';
+import {
+  ApiPropertyEnum,
+  ApiPropertyUUID,
+} from 'src/utils/swagger/dto-decorators';
 import { FieldExists, ValidationConstraints } from 'src/validators/FieldExists';
 
 export class CreateGroupMemberDto {
   @IsRequiredString()
-  @ApiProperty({ default: generateUUID() })
+  @ApiPropertyUUID()
   @Validate(FieldExists, ['group', 'uuid'] as ValidationConstraints)
   group_uuid: string;
 
   @IsRequiredString()
-  @ApiProperty({ default: generateUUID() })
+  @ApiPropertyUUID()
   @Validate(FieldExists, ['user', 'uuid'] as ValidationConstraints)
   user_uuid: string;
 
   @IsRequiredEnum(RoleEnum)
-  @ApiProperty({ enum: RoleEnum })
+  @ApiPropertyEnum({ Enum: RoleEnum, Default: RoleEnum.Admin })
   role: RoleEnum;
 }
