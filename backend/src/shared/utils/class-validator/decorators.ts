31f74b94ba4447aa22e906c2df5aaccc095c15c8
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-10 09:57:55 -0300

    Refactor validators and decorators for class-validator

--- backend/src/shared/utils/class-validator/decorators.ts
+++ backend/src/shared/utils/class-validator/decorators.ts
@@ -1,4 +1,16 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  IsUrl,
+  Validate,
+} from 'class-validator';
+import { FieldExists } from './validators/FieldExists';
+import { Prisma } from '@prisma/client';
+
+type UncapitalizedModelName = Uncapitalize<Prisma.ModelName>;
 
 function IsRequiredString() {
   return function (object: Record<string, any>, propertyName: string) {
@@ -14,4 +26,38 @@ function IsRequiredEnum(Enum: object) {
   };
 }
 
-export { IsRequiredString, IsRequiredEnum };
+function IsRequiredUUID() {
+  return function (object: Record<string, any>, propertyName: string) {
+    IsUUID()(object, propertyName);
+    IsNotEmpty()(object, propertyName);
+  };
+}
+
+function IsRequiredEmail() {
+  return function (object: Record<string, any>, propertyName: string) {
+    IsEmail()(object, propertyName);
+    IsNotEmpty()(object, propertyName);
+  };
+}
+
+function IsRequiredURL() {
+  return function (object: Record<string, any>, propertyName: string) {
+    IsUrl()(object, propertyName);
+    IsNotEmpty()(object, propertyName);
+  };
+}
+
+function ValidateFieldExists(model: UncapitalizedModelName, field: string) {
+  return function (object: Record<string, any>, propertyName: string) {
+    Validate(FieldExists, [model, field])(object, propertyName);
+  };
+}
+
+export {
+  IsRequiredString,
+  IsRequiredEnum,
+  IsRequiredUUID,
+  ValidateFieldExists,
+  IsRequiredEmail,
+  IsRequiredURL,
+};
