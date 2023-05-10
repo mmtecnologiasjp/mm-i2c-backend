5b14095996ff03b49bbcfe08f23e19b7b4fd8872
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-10 09:54:14 -0300

    Refactor user creation to check for existing email

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -6,25 +6,22 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaError } from 'prisma-error-enum';
-import { Prisma } from '@prisma/client';
+import { tryCatch } from 'src/shared/utils/tryCatch';
 
 @Injectable()
 export class UsersService {
   async create(createUserDto: CreateUserDto) {
-    try {
-      const userCreated = await prisma.user.create({
-        data: createUserDto,
-      });
+    const user = await this._getUserByEmail(createUserDto.email);
 
-      return userCreated;
-    } catch (error) {
-      const prismaError = error as Prisma.PrismaClientKnownRequestError;
+    const userExists = !!user;
 
-      if (prismaError.code === PrismaError.UniqueConstraintViolation) {
-        throw new ConflictException('Email already exists');
-      }
-    }
+    if (userExists) throw new ConflictException('Email already exists');
+
+    const userCreatedPromise = prisma.user.create({ data: createUserDto });
+
+    const [data] = await tryCatch(userCreatedPromise);
+
+    return data;
   }
 
   findAll() {
@@ -42,7 +39,7 @@ export class UsersService {
   async update(uuid: string, updateUserDto: UpdateUserDto) {
     const user = await this._getUser(uuid);
 
-    if (!user) throw new NotFoundException();
+    if (!user) throw new NotFoundException('User not found');
 
     return prisma.user.update({
       where: { uuid: user.uuid },
@@ -64,6 +61,13 @@ export class UsersService {
   private _getUser(uuid: string) {
     return prisma.user.findUnique({
       where: { uuid },
+    });
+  }
+
+  private _getUserByEmail(email: string) {
+    return prisma.user.findUnique({
+      where: { email },
+      select: { email: true },
     });
   }
 }
