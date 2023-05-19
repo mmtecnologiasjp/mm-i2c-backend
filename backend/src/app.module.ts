510fc43d5cb46f4d75e413723fcc1d0abad48eaa
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-19 14:15:45 -0300

    added message module to app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './modules/users/users.module';
 import { GroupsModule } from './modules/groups/groups.module';
 import { GroupMembersModule } from './modules/group-members/group-members.module';
 import { PrivateConversationsModule } from './modules/private-conversations/private-conversations.module';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrivateConversationsModule } from './modules/private-conversations/priv
     GroupsModule,
     PrivateConversationsModule,
     GroupMembersModule,
+    MessagesModule,
   ],
 })
 export class AppModule {}
