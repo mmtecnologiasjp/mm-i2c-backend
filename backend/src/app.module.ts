6d139b8fcc60d915170d8388726ff667b688a4fc
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-19 14:13:23 -0300

    better path for swagger and colorizing logs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { GroupsModule } from './modules/groups/groups.module';
 import { GroupMembersModule } from './modules/group-members/group-members.module';
-import { PrivateConversationsModule } from './private-conversations/private-conversations.module';
+import { PrivateConversationsModule } from './modules/private-conversations/private-conversations.module';
 
 @Module({
   imports: [
