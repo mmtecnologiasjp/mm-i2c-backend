0e89efed07587fca1c4ca5e94c639c83181401ff
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-19 14:04:03 -0300

    changed swagger path and colorize logs of running server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,12 +22,25 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  const path = 'api';
+  const path = 'docs';
   SwaggerModule.setup(path, app, document);
 
   const PORT = process.env.PORT ?? 4000;
-  await app.listen(PORT, () =>
-    console.log(`[MM CHAT] RUNNING ON PORT ${PORT}`),
-  );
+  const url = `http://localhost:${PORT}`;
+
+  const BLUE_COLOR = `\x1b[34m`;
+  const GREEN_COLOR = `\x1b[32m`;
+  const WHITE_COLOR = `\x1b[97m`;
+  const END_COLOR = '\x1b[0m';
+
+  await app.listen(PORT, () => {
+    console.log(
+      `\n${BLUE_COLOR}[MM CHAT]${END_COLOR} RUNNING AT ${WHITE_COLOR}${url}/`,
+    );
+    console.log(
+      `${GREEN_COLOR}[SWAGGER]${END_COLOR} RUNNING AT ${WHITE_COLOR}${url}/${path}`,
+    );
+  });
 }
+
 bootstrap();
