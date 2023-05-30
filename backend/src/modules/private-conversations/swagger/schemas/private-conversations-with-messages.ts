d817c20d8b4b5f6970d6dbcdd5d5fe2a83cd02f4
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-30 11:46:09 -0300

    feat(swagger): add tasks to private conversation schema

--- backend/src/modules/private-conversations/swagger/schemas/private-conversations-with-messages.ts
+++ backend/src/modules/private-conversations/swagger/schemas/private-conversations-with-messages.ts
@@ -1,8 +1,12 @@
 import { Message } from 'src/modules/messages/entities/message.entity';
 import { PrivateConversation } from '../../entities/private-conversation.entity';
 import { ApiProperty } from '@nestjs/swagger';
+import { Task } from 'src/modules/tasks/entities/task.entity';
 
-export class PrivateConversationWithMessages extends PrivateConversation {
+export class PrivateConversationWithMessagesAndTasks extends PrivateConversation {
   @ApiProperty({ type: Message, isArray: true })
   messages: Message[];
+
+  @ApiProperty({ type: Task, isArray: true })
+  tasks: Task[];
 }
