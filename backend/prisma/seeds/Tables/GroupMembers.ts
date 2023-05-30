47bc012585ffde53029345d872fd8a11059dfea3
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-30 13:21:21 -0300

    chore: removed code commented

--- backend/prisma/seeds/Tables/GroupMembers.ts
+++ backend/prisma/seeds/Tables/GroupMembers.ts
@@ -27,18 +27,5 @@ export class GroupMembersSeeds {
     await prisma.groupMember.createMany({ data: groupMembers });
 
     console.log(`Created ${groupMembersUuid.length} Group Members`);
-
-    // for (let i = 0; i < 2; i++) {
-    //   const role = i === 0 ? RoleEnum.Admin : RoleEnum.Member;
-
-    //   const groupMember = await prisma.groupMember.create({
-    //     data: {
-    //       group_uuid: `01`,
-    //       user_uuid: `0${i + 1}`,
-    //       role,
-    //     },
-    //   });
-    //   console.log(`Created groupMember with id: ${groupMember.uuid}`);
-    // }
   }
 }
