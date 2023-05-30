56585e0babc6a0d70238a91d1fc62f240417db92
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-30 11:56:57 -0300

    feat(seeds): add initial seed data for users, groups, group members, private conversations, and messages

--- backend/prisma/seeds/Tables/GroupMembers.ts
+++ backend/prisma/seeds/Tables/GroupMembers.ts
@@ -1,19 +1,44 @@
-import { PrismaClient, RoleEnum } from '@prisma/client';
+import { Group, GroupMember, PrismaClient, RoleEnum } from '@prisma/client';
+import { OmitTimestamps } from '..';
+import { groupsUuid } from './Groups';
+import { userUuids } from './Users';
 const prisma = new PrismaClient();
+
+const groupMembersUuid = [
+  '3aaf14d5-3e06-4c8e-b682-9fb623c63897',
+  '2953e954-12be-4f3e-9e26-43b2db4960a7',
+  '1f5a4227-894f-4a46-af60-7a2d1b07e8a1',
+  '0e240168-8939-4a37-8c3a-5e6f03b982a7',
+];
 
 export class GroupMembersSeeds {
   static async execute() {
-    for (let i = 0; i < 2; i++) {
-      const role = i === 0 ? RoleEnum.Admin : RoleEnum.Member;
+    const groupMembers = [] as OmitTimestamps<GroupMember>[];
 
-      const groupMember = await prisma.groupMember.create({
-        data: {
-          group_uuid: `01`,
-          user_uuid: `0${i + 1}`,
-          role,
-        },
+    for (let i = 0; i < groupMembersUuid.length; i++) {
+      groupMembers.push({
+        uuid: groupMembersUuid[i],
+        group_uuid: groupsUuid[i],
+        role: 'Admin',
+        user_uuid: userUuids[i],
       });
-      console.log(`Created groupMember with id: ${groupMember.uuid}`);
     }
+
+    await prisma.groupMember.createMany({ data: groupMembers });
+
+    console.log(`Created ${groupMembersUuid.length} Group Members`);
+
+    // for (let i = 0; i < 2; i++) {
+    //   const role = i === 0 ? RoleEnum.Admin : RoleEnum.Member;
+
+    //   const groupMember = await prisma.groupMember.create({
+    //     data: {
+    //       group_uuid: `01`,
+    //       user_uuid: `0${i + 1}`,
+    //       role,
+    //     },
+    //   });
+    //   console.log(`Created groupMember with id: ${groupMember.uuid}`);
+    // }
   }
 }
--- backend/prisma/seeds/Tables/Users.ts
+++ backend/prisma/seeds/Tables/Users.ts
@@ -1,27 +1,44 @@
+import { User } from '@prisma/client';
 import prisma from '../../../src/client';
 import { faker } from '@faker-js/faker';
+import { OmitTimestamps } from '..';
+
+export const userUuids = [
+  'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+  'e6310bc6-8e6c-4fc0-b320-73cfb4c40e60',
+  'd57aebc5-8c29-44b0-a64e-0c2f1736b82b',
+  'c45de99e-d437-4ebc-a714-ae0e80b3c9a1',
+  'b34a6e9b-1070-4c62-94ca-1a68d947da6d',
+  'a23cd78e-3a01-43c7-b0d1-21e9f5676a2f',
+  '9190b69f-68cd-4c8d-9716-27d6d48b2c7d',
+  '8253a4d1-7a9b-4c11-843f-3dcd5c3a52e7',
+  '7302670c-3f07-4dd2-b023-4e7e010249ff',
+  '6269dd84-8e92-4f47-8a0d-9a5b6c3b2458',
+];
 
 export class UsersSeeds {
   static async execute() {
-    for (let i = 0; i < 10; i++) {
-      const user = await prisma.user.create({
-        data: {
-          uuid: `0${i + 1}`,
-          first_name: faker.name.firstName(),
-          last_name: faker.name.lastName(),
-          username: faker.internet.userName(),
-          email: faker.internet.email(),
-          status: faker.helpers.arrayElement([
-            'Active',
-            'Inactive',
-            'Banned',
-            'Quarantine',
-          ]),
-          password: faker.internet.password(),
-          avatar_url: faker.internet.avatar(),
-        },
+    const users = [] as OmitTimestamps<User>[];
+
+    for (let i = 0; i < userUuids.length; i++) {
+      users.push({
+        uuid: userUuids[i],
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        status: faker.helpers.arrayElement([
+          'Active',
+          'Inactive',
+          'Banned',
+          'Quarantine',
+        ]),
+        password: faker.internet.password(),
+        avatar_url: faker.internet.avatar(),
       });
-      console.log(`Created user with id: ${user.uuid}`);
     }
+
+    await prisma.user.createMany({ data: users });
+    console.log(`Created ${userUuids.length} Users`);
   }
 }
