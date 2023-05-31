b797ad46ebb6bdf3d0ef1ad8b563ad88d6230ddc
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-31 10:53:54 -0300

    chore(group): new name for groupMessage schema class

--- backend/src/modules/groups/swagger/schemas/group-messages-tasks.ts
+++ backend/src/modules/groups/swagger/schemas/group-messages-tasks.ts
@@ -3,7 +3,7 @@ import { Task } from 'src/modules/tasks/entities/task.entity';
 import { Group } from '../../entities/group.entity';
 import { MessageSender } from '../../../../shared/schemas/MessageSender';
 
-export class GroupMessages extends Group {
+export class GroupMessagesTasks extends Group {
   @ApiProperty({ type: MessageSender, isArray: true })
   messages: MessageSender[];
 
