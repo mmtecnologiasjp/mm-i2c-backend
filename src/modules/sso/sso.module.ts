eacbd8bfa48f5d88deedc62e1c04ecc2f83b455c
ChristoferMendes <christoferluizdsm@gmail.com>
2025-03-07 09:22:04 -0300

    feat: add SsoMicroservicesApiModule to SSO module imports

--- src/modules/sso/sso.module.ts
+++ src/modules/sso/sso.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SsoService } from './sso.service';
 import { SsoApiModule } from '../sso-api/sso-api.module';
+import { SsoMicroservicesApiModule } from '../sso-microservices-api/sso-microservices-api.module';
 
 @Module({
-  imports: [SsoApiModule],
+  imports: [SsoApiModule, SsoMicroservicesApiModule],
   providers: [SsoService],
   exports: [SsoService],
 })
